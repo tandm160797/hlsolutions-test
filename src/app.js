54c8faa422789dda31f69c3befa8bd46f2848444
Tan Dang <tandm160797@gmail.com>
2021-04-17 20:39:38 +0700

    Create project structure

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+
+(async () => {
+	const app = express();
+	const host = process.env.HOST || 'localhost';
+	const port = process.env.PORT || 3000;
+
+	app.set('view engine', 'pug');
+	app.set('views', path.join(path.resolve(), 'src/resources/views'));
+
+	app.use(morgan('dev'));
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: true }));
+	app.use(express.static(path.join(path.resolve(), 'public')));
+
+	app.listen(port, () => {
+		console.log(`App listening at ${host}:${port}`);
+	});
+})();
